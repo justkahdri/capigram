b0c95d895af862e55ee8b68e5c07217611a5eb33
Joaquín R. Montes <joaco.rm512@gmail.com>
2021-07-24 00:51:47 -0300

    fixed theme & added global types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,12 +1,27 @@
 declare global {
+  // COMPONENT PROPS
+  interface StoryProps {
+    cover: string;
+    emoji: string;
+    name: string;
+    path: string;
+  }
+
+  interface PostProps {
+    id: number;
+    likes?: number;
+    src: string;
+  }
+
+  // USE CASES
+  interface TCategory extends StoryProps {
+    id: number;
+  }
+
   interface TUser {
     nickname: string;
     name: string;
     avatarUrl: string;
-  }
-  interface StoryProps {
-    user: TUser;
-    route: string;
   }
 }
 
