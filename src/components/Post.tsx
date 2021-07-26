443256dc92286071838a8563545104fb51cfa9e4
Joaquín R. Montes <joaco.rm512@gmail.com>
2021-07-26 20:03:01 -0300

    Likes on posts saved on local storage

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,15 +1,25 @@
+import React from "react";
 import { Flex, Image, Skeleton, Stack, Text, Icon } from "@chakra-ui/react";
 import Link from "next/link";
-import React from "react";
 
-import { BiHeart } from "react-icons/bi";
+import useLocalStorage from "@hooks/useLocalStorage";
+
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 
 const Post = ({ id, likes = 0, src }: PostProps) => {
+  const key = `like-${id}`;
+  const [liked, setLiked] = useLocalStorage(key, false);
+
   return (
     <Flex flexDir="column" as="article" width="100%">
       <Image src={src} objectFit="cover" fallback={<Skeleton minH="75vw" />} />
-      <Stack direction="row" alignItems="center">
-        <Icon as={BiHeart} boxSize={6} />
+      <Stack
+        direction="row"
+        alignItems="center"
+        onClick={() => setLiked(!liked)}
+        width="fit-content"
+      >
+        <FavIcon liked={liked} />
         <Text fontWeight={800}>
           {likes} {likes == 1 ? "Like" : "Likes"}
         </Text>
@@ -25,5 +35,12 @@ const Post = ({ id, likes = 0, src }: PostProps) => {
     </Flex>
   );
 };
+
+const FavIcon = (props: { liked: boolean }) =>
+  props.liked ? (
+    <Icon as={AiFillHeart} boxSize={6} color="red.500" />
+  ) : (
+    <Icon as={AiOutlineHeart} boxSize={6} />
+  );
 
 export default Post;
---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { useToast } from "@chakra-ui/react";
+
+function useLocalStorage<Type>(key: string, initialValue: Type) {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const value = window.localStorage.getItem(key);
+      if (value == null) {
+        throw new Error("Invalid value from local storage");
+      }
+      const parsedValue = JSON.parse(value);
+      if (typeof parsedValue == typeof initialValue) {
+        return parsedValue as Type;
+      } else return initialValue;
+    } catch (err) {
+      return initialValue;
+    }
+  });
+
+  const toast = useToast({
+    title: "Ups! Try again later...",
+    status: "error",
+    isClosable: true,
+    // duration: 5000 default
+  });
+
+  const setLocalStorage = (value: Type) => {
+    try {
+      window.localStorage.setItem(key, JSON.stringify(value));
+      setStoredValue(value);
+    } catch (err) {
+      toast({ description: err.message });
+    }
+  };
+
+  return [storedValue, setLocalStorage] as [Type, (new_value: Type) => void];
+}
+
+export default useLocalStorage;
